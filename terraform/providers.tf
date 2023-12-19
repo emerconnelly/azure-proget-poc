40ee0a9cb5590a12ade4877008326bccee9b30de
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-19 11:20:19 -0600

    init

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.6.6"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.84"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
