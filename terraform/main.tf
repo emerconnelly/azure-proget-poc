7118bd4f5e66d5441131b5acca7405773dd1d29b
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-19 17:02:55 -0600

    Update main.tf with Azure Kubernetes Service configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "azure_proget_poc" {
 resource "azurerm_storage_account" "proget_packages" {
   name = "azureprogetpocpackages"
 
-  resource_group_name      = "azure-proget-poc"
+  resource_group_name      = resource.azurerm_resource_group.azure_proget_poc.name
   location                 = "centralus"
   account_kind             = "StorageV2"
   account_tier             = "Standard"
@@ -29,4 +29,32 @@ resource "azurerm_storage_container" "proget_packages" {
   container_access_type = "private"
 
   depends_on = [azurerm_storage_account.proget_packages]
+}
+
+resource "azurerm_kubernetes_cluster" "azure_proget_poc" {
+  name                = "azure-proget-poc"
+  resource_group_name = resource.azurerm_resource_group.azure_proget_poc.name
+  location            = "centralus"
+
+  dns_prefix                = "azure-proget-poc-dns"
+  sku_tier                  = "Free"
+  automatic_channel_upgrade = "patch"
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_DS2_v2"
+    node_count = 1
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+    network_policy    = "calico"
+  }
+
+  depends_on = [azurerm_resource_group.azure_proget_poc]
 }
