bb956396f04fb7feedddce94f9bca305be98076d
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-19 15:44:57 -0600

    Refactor storage container resources in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,32 @@
 resource "azurerm_resource_group" "azure_proget_poc" {
-  name     = "azure-proget-poc"
+  name = "azure-proget-poc"
+
   location = "centralus"
 }
 
 resource "azurerm_storage_account" "proget_packages" {
-  name                = "azureprogetpocpackages"
-  location            = "centralus"
-  resource_group_name = "azure-proget-poc"
+  name = "azureprogetpocpackages"
 
+  resource_group_name      = "azure-proget-poc"
+  location                 = "centralus"
   account_kind             = "BlobStorage"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   access_tier              = "Cool"
+
+  depends_on = [azurerm_resource_group.azure_proget_poc]
 }
 
-resource "azurerm_storage_container" "npm_azure_storage" {
-  name                  = "npm-azure-storage"
-  storage_account_name  = resource.azurerm_storage_account.proget_packages.name
-  container_access_type = "private"
-}
+resource "azurerm_storage_container" "proget_packages" {
+  for_each = toset([
+    "npm-azure-storage",
+    "dotnet-azure-storage",
+    "chocolatey-azure-storage",
+  ])
+  name = each.key
 
-resource "azurerm_storage_container" "dotnet_azure_storage" {
-  name                  = "dotnet-azure-storage"
   storage_account_name  = resource.azurerm_storage_account.proget_packages.name
   container_access_type = "private"
-}
 
-resource "azurerm_storage_container" "chocolatey_azure_storage" {
-  name                  = "chocolatey-azure-storage"
-  storage_account_name  = resource.azurerm_storage_account.proget_packages.name
-  container_access_type = "private"
+  depends_on = [azurerm_storage_account.proget_packages]
 }
