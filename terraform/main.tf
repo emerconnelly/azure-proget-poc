046fb627ee96c1fcfc5b8c54c1130fa9a2339ed5
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-19 12:25:30 -0600

    Update storage account names in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,18 +16,18 @@ resource "azurerm_storage_account" "proget_packages" {
 
 resource "azurerm_storage_container" "npm_azure_storage" {
   name                  = "npm-azure-storage"
-  storage_account_name  = resource.azurerm_storage_account.proget_poc_packages.name
+  storage_account_name  = resource.azurerm_storage_account.proget_packages.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "dotnet_azure_storage" {
   name                  = "dotnet-azure-storage"
-  storage_account_name  = resource.azurerm_storage_account.proget_poc_packages.name
+  storage_account_name  = resource.azurerm_storage_account.proget_packages.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "chocolatey_azure_storage" {
   name                  = "chocolatey-azure-storage"
-  storage_account_name  = resource.azurerm_storage_account.proget_poc_packages.name
+  storage_account_name  = resource.azurerm_storage_account.proget_packages.name
   container_access_type = "private"
 }
