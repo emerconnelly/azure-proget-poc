c6e532fca7514f7d72f8bbe0306b77f777def0ca
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-19 19:33:14 -0600

    bunch of stuff lol

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,28 @@
-resource "azurerm_resource_group" "azure_proget_poc" {
+resource "random_integer" "this" {
+  min = 100000000000000
+  max = 999999999999999
+}
+
+resource "azurerm_resource_group" "this" {
   name = "azure-proget-poc"
 
   location = "centralus"
 }
 
-resource "azurerm_storage_account" "proget_packages" {
-  name = "azureprogetpocpackages"
+resource "azurerm_storage_account" "this" {
+  name = substr("progetpackages${resource.random_integer.this.result}}", 0, 24)
 
-  resource_group_name      = resource.azurerm_resource_group.azure_proget_poc.name
+  resource_group_name      = resource.azurerm_resource_group.this.name
   location                 = "centralus"
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   access_tier              = "Cool"
 
-  depends_on = [azurerm_resource_group.azure_proget_poc]
+  depends_on = [resource.azurerm_resource_group.this]
 }
 
-resource "azurerm_storage_container" "proget_packages" {
+resource "azurerm_storage_container" "this" {
   for_each = toset([
     "npm-azure-storage",
     "dotnet-azure-storage",
@@ -25,29 +30,29 @@ resource "azurerm_storage_container" "proget_packages" {
   ])
   name = each.key
 
-  storage_account_name  = resource.azurerm_storage_account.proget_packages.name
+  storage_account_name  = resource.azurerm_storage_account.this.name
   container_access_type = "private"
 
-  depends_on = [azurerm_storage_account.proget_packages]
+  depends_on = [resource.azurerm_storage_account.this]
 }
 
 resource "azurerm_virtual_network" "this" {
   name                = "azure-proget-poc-vnet"
-  resource_group_name = resource.azurerm_resource_group.azure_proget_poc.name
+  resource_group_name = resource.azurerm_resource_group.this.name
   location            = "centralus"
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "aks" {
   name                 = "aks"
-  resource_group_name  = resource.azurerm_resource_group.azure_proget_poc.name
+  resource_group_name  = resource.azurerm_resource_group.this.name
   virtual_network_name = resource.azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_subnet" "app_gateway" {
   name                 = "app-gateway"
-  resource_group_name  = resource.azurerm_resource_group.azure_proget_poc.name
+  resource_group_name  = resource.azurerm_resource_group.this.name
   virtual_network_name = resource.azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/24"]
 }
