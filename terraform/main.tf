1e2b50e59a1294ad3f192c83df3785cc79be9a7f
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-20 15:36:49 -0600

    Add storage account and storage containers

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,28 +9,28 @@ resource "azurerm_resource_group" "this" {
   location = "centralus"
 }
 
-# resource "azurerm_storage_account" "this" {
-#   name = substr("progetpackages${resource.random_integer.this.result}}", 0, 24)
+resource "azurerm_storage_account" "this" {
+  name = substr("progetpackages${resource.random_integer.this.result}}", 0, 24)
 
-#   resource_group_name      = resource.azurerm_resource_group.this.name
-#   location                 = "centralus"
-#   account_kind             = "StorageV2"
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   access_tier              = "Cool"
-# }
+  resource_group_name      = resource.azurerm_resource_group.this.name
+  location                 = "centralus"
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Cool"
+}
 
-# resource "azurerm_storage_container" "this" {
-#   for_each = toset([
-#     "npm-azure-storage",
-#     "dotnet-azure-storage",
-#     "chocolatey-azure-storage",
-#   ])
-#   name = each.key
+resource "azurerm_storage_container" "this" {
+  for_each = toset([
+    "npm-azure-storage",
+    "dotnet-azure-storage",
+    "chocolatey-azure-storage",
+  ])
+  name = each.key
 
-#   storage_account_name  = resource.azurerm_storage_account.this.name
-#   container_access_type = "private"
-# }
+  storage_account_name  = resource.azurerm_storage_account.this.name
+  container_access_type = "private"
+}
 
 # resource "azurerm_virtual_network" "this" {
 #   name                = "azure-proget-poc-vnet"
