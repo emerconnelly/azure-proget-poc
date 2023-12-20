eb2c84d9d3369fec2a703c253de6b4f4b2b3243c
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-19 23:20:00 -0600

    Add Flux extension to AKS cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,3 +91,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
     dns_service_ip    = "172.16.0.10"
   }
 }
+
+resource "azurerm_kubernetes_cluster_extension" "flux" {
+  name           = "microsoft.flux"
+  cluster_id     = azurerm_kubernetes_cluster.aks.id
+  extension_type = "microsoft.flux"
+}
