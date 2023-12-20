949362fef56c5250438c147f9cac21719943133e
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-19 19:11:25 -0600

    Update subnet name for app gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,36 +46,36 @@ resource "azurerm_subnet" "aks" {
 }
 
 resource "azurerm_subnet" "app_gateway" {
-  name                 = "aks"
+  name                 = "app-gateway"
   resource_group_name  = resource.azurerm_resource_group.azure_proget_poc.name
   virtual_network_name = resource.azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0./24"]
 }
 
-resource "azurerm_kubernetes_cluster" "azure_proget_poc" {
-  name                = "azure-proget-poc"
-  resource_group_name = resource.azurerm_resource_group.azure_proget_poc.name
-  location            = "centralus"
+# resource "azurerm_kubernetes_cluster" "azure_proget_poc" {
+#   name                = "azure-proget-poc"
+#   resource_group_name = resource.azurerm_resource_group.azure_proget_poc.name
+#   location            = "centralus"
 
-  dns_prefix                = "azure-proget-poc-dns"
-  sku_tier                  = "Free"
-  automatic_channel_upgrade = "patch"
+#   dns_prefix                = "azure-proget-poc-dns"
+#   sku_tier                  = "Free"
+#   automatic_channel_upgrade = "patch"
 
-  default_node_pool {
-    name       = "agentpool"
-    vm_size    = "Standard_DS2_v2"
-    node_count = 1
-  }
+#   default_node_pool {
+#     name       = "agentpool"
+#     vm_size    = "Standard_DS2_v2"
+#     node_count = 1
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = "standard"
-    network_policy    = "calico"
-  }
+#   network_profile {
+#     network_plugin    = "kubenet"
+#     load_balancer_sku = "standard"
+#     network_policy    = "calico"
+#   }
 
-  depends_on = [azurerm_resource_group.azure_proget_poc]
-}
+#   depends_on = [azurerm_resource_group.azure_proget_poc]
+# }
