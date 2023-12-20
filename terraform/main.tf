f3c5a0c00eae744c6ec2bed4e2116e0ae759147b
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-19 19:08:33 -0600

    Add virtual network and subnets for AKS and app gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,6 +31,27 @@ resource "azurerm_storage_container" "proget_packages" {
   depends_on = [azurerm_storage_account.proget_packages]
 }
 
+resource "azurerm_virtual_network" "this" {
+  name                = "azure-proget-poc-vnet"
+  resource_group_name = resource.azurerm_resource_group.azure_proget_poc.name
+  location            = "centralus"
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "aks"
+  resource_group_name  = resource.azurerm_resource_group.azure_proget_poc.name
+  virtual_network_name = resource.azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.0.0./24"]
+}
+
+resource "azurerm_subnet" "app_gateway" {
+  name                 = "aks"
+  resource_group_name  = resource.azurerm_resource_group.azure_proget_poc.name
+  virtual_network_name = resource.azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.1.0./24"]
+}
+
 resource "azurerm_kubernetes_cluster" "azure_proget_poc" {
   name                = "azure-proget-poc"
   resource_group_name = resource.azurerm_resource_group.azure_proget_poc.name
