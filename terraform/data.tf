df5685cd9ee5c84fce92b9e9773fb8ad1ebdb3db
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-20 21:21:22 -0600

    echo the actual value

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,3 @@
 data "external" "azure_service_principal" {
-  program = ["bash", "-c", "echo '{ \"arm_client_secret\": \"$ARM_CLIENT_SECRET\", \"arm_client_id\": \"$ARM_CLIENT_ID\" }'"]
+  program = ["bash", "-c", "echo '{ \"arm_client_secret\": \"$(echo $ARM_CLIENT_SECRET)\", \"arm_client_id\": \"$(echo $ARM_CLIENT_ID)\" }'"]
 }
