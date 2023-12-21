c43a5d1e595d8f4277cc8296d185ffd95d333857
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-20 21:02:44 -0600

    Update ARM client ID and secret in data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,7 +1,7 @@
-data "external" "arm_client_id" {
-  program = ["bash", "-c", "echo -n $ARM_CLIENT_ID"]
+data "external" "arm_client_secret" {
+  program = ["bash", "-c", "echo '{ \"arm_client_secret\": \"$ARM_CLIENT_SECRET\" }'"]
 }
 
-data "external" "arm_client_secret" {
-  program = ["bash", "-c", "echo -n $ARM_CLIENT_SECRET"]
+data "external" "arm_client_id" {
+  program = ["bash", "-c", "echo '{ \"arm_client_id\": \"$ARM_CLIENT_ID\" }'"]
 }
