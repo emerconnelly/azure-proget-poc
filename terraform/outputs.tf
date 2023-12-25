2924b0840eafcd3cd4647bbd0730aa755fbd07c1
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-25 17:52:44 -0600

    Add output for AKS secret store secret identity

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,10 @@
+output "aks_secret_store_secret_identity" {
+  value = resource.azurerm_kubernetes_cluster.aks.key_vault_secrets_provider.secret_identity
+  description = "The identity used for the Secret Store CSI driver for Azure Key Vault."
+
+  # sensitive = true
+}
+
 output "kubeconfig" {
   value       = resource.azurerm_kubernetes_cluster.aks.kube_config_raw
   description = "Kubeconfig for AKS cluster"
