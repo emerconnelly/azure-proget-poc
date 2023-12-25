f9fe47b4784c4f3bac45a9a713cd9f253d5bff1d
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-24 22:14:58 -0600

    Update resource locations in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_storage_account" "this" {
   name = substr("progetpackages${resource.random_integer.this.result}}", 0, 24)
 
   resource_group_name      = resource.azurerm_resource_group.this.name
-  location                 = "centralus"
+  location                 = resource.azurerm_resource_group.this.location
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -35,7 +35,7 @@ resource "azurerm_storage_container" "this" {
 resource "azurerm_virtual_network" "this" {
   name                = "azure-proget-poc-vnet"
   resource_group_name = resource.azurerm_resource_group.this.name
-  location            = "centralus"
+  location            = resource.azurerm_resource_group.this.location
   address_space       = ["10.0.0.0/16"]
 }
 
@@ -54,19 +54,32 @@ resource "azurerm_subnet" "app_gateway" {
 }
 
 resource "azurerm_mssql_server" "this" {
-  name                         = "proget-${resource.random_integer.this.result}"
-  resource_group_name          = resource.azurerm_resource_group.this.name
-  location                     = resource.azurerm_resource_group.this.location
+  name                = "proget-${resource.random_integer.this.result}"
+  resource_group_name = resource.azurerm_resource_group.this.name
+  location            = resource.azurerm_resource_group.this.location
+
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "thisIsKat11"
   minimum_tls_version          = "1.2"
 }
 
+resource "azurerm_mssql_database" "this" {
+  name = "proget"
+
+  server_id                   = resource.azurerm_mssql_server.this.id
+  sku_name                    = "GP_S_Gen5_1"
+  collation                   = "SQL_Latin1_General_CP1_CI_AS"
+  storage_account_type        = "Local"
+  max_size_gb                 = 5
+  min_capacity                = 0.5
+  auto_pause_delay_in_minutes = 60
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks"
   resource_group_name = "azure-proget-poc"
-  location            = "centralus"
+  location            = resource.azurerm_resource_group.this.location
 
   # automatic_channel_upgrade           = "patch"
   dns_prefix = "aks-dns"
@@ -151,5 +164,5 @@ resource "azurerm_kubernetes_flux_configuration" "proget" {
     sync_interval_in_seconds   = 60
   }
 
-  depends_on = [ resource.azurerm_mssql_server.this ]
+  depends_on = [resource.azurerm_mssql_server.this]
 }
