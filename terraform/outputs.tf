6128c2faa5882e8998941b8d08304386fd2e0c71
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-24 22:26:33 -0600

    Add output for SQL connection string

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,3 +4,10 @@ output "kubeconfig" {
 
   sensitive = true
 }
+
+output "sql_connection_string" {
+  value       = "Server=tcp:${resource.azurerm_sql_server.this.fully_qualified_domain_name},1433;Initial Catalog=${resource.azurerm_sql_database.this.name};Persist Security Info=False;User ID=${resource.azurerm_sql_database.this.administrator_login};Password=${resource.azurerm_sql_database.this.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  description = "Connection string for the Azure SQL Database created."
+
+  sensitive = true
+}
