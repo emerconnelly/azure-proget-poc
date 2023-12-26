aea803c87a477443d6d8081c030a46cdd6f686d3
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-25 18:04:48 -0600

    Update output name for CSI secret store identity

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "aks_secret_store_secret_identity" {
+output "csi_secret_store_secret_identity" {
   value = resource.azurerm_kubernetes_cluster.aks.key_vault_secrets_provider[0].secret_identity
   description = "The identity used for the Secret Store CSI driver for Azure Key Vault."
 
