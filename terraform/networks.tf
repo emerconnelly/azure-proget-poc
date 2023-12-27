29a3fe55725c38de94d6ef6c72b2b482a1d645b8
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-27 11:30:42 -0600

    Update virtual network name in networks.tf

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "this" {
-  name                = "${azurerm_resource_group.this.name}-vnet"
+  name                = "${azurerm_resource_group.this.name}"
   resource_group_name = azurerm_resource_group.this.name
 
   location      = azurerm_resource_group.this.location
