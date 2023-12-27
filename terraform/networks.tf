279cde10e2a7b76d4e36ae90c429f255e5612ba8
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-27 10:09:32 -0600

    Refactor network configurations

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -1,26 +1,28 @@
 resource "azurerm_virtual_network" "this" {
   name                = "azure-proget-poc-vnet"
   resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  address_space       = ["10.0.0.0/16"]
+
+  location      = azurerm_resource_group.this.location
+  address_space = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "aks_nodes" {
-  name                 = "aks"
-  resource_group_name  = azurerm_resource_group.this.name
+  name                = "aks-nodes"
+  resource_group_name = azurerm_resource_group.this.name
+
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.0.0/24"]
-
   service_endpoints = [
     "Microsoft.Sql",
     "Microsoft.Storage",
     "Microsoft.KeyVault",
-    ]
+  ]
 }
 
 resource "azurerm_subnet" "app_gateway" {
-  name                 = "app-gateway"
-  resource_group_name  = azurerm_resource_group.this.name
+  name                = "app-gateway"
+  resource_group_name = azurerm_resource_group.this.name
+
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/24"]
 }
