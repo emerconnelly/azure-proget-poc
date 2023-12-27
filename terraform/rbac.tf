e7b745681e01d64182c6855e01afd3c737a36f1f
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-27 11:15:25 -0600

    Remove unnecessary role assignment name in rbac.tf

--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -1,6 +1,4 @@
 resource "azurerm_role_assignment" "key_vault_reader" {
-  name = "key-vault-reader"
-
   scope              = azurerm_key_vault.this.id
   role_definition_id = data.azurerm_role_definition.key_vault_reader.id
   principal_id       = data.azurerm_client_config.this.object_id
