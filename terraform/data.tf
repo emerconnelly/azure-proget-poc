eff54fcad1c8f1076bf50708c6bb27aa5db9aa52
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-27 14:26:44 -0600

    Update key vault role definition to key_vault_administrator

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,7 +2,7 @@ data "azurerm_subscription" "this" {}
 
 data "azurerm_client_config" "this" {}
 
-data "azurerm_role_definition" "key_vault_reader" {
+data "azurerm_role_definition" "key_vault_administrator" {
   name  = "Key Vault Secrets Officer"
   scope = azurerm_key_vault.this.id
 }
