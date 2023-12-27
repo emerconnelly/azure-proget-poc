eaca22ea00f2386caa38dc9e936b44661d2e5406
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-27 13:18:55 -0600

    Update role definition name in data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,6 +3,6 @@ data "azurerm_subscription" "this" {}
 data "azurerm_client_config" "this" {}
 
 data "azurerm_role_definition" "key_vault_reader" {
-  name  = "Key Vault Reader"
+  name  = "Key Vault Secrets Officer"
   scope = azurerm_key_vault.this.id
 }
