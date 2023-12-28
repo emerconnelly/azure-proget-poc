2c90bca4a2e1e61427ba1e8b9a9e44b5614cf0f7
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-28 11:39:38 -0600

    Add azurerm_federated_identity_credential resource for AKS CSI driver

--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -16,8 +16,18 @@ resource "azurerm_role_assignment" "tf_azurerm" {
 #   principal_id       = azurerm_user_assigned_identity.aks_csi_driver_managed_identity.principal_id
 # }
 
-resource "azurerm_role_assignment" "aks_csi_driver_proget" {
+resource "azurerm_role_assignment" "aks_csi_driver_managed_identity" {
   scope              = azurerm_key_vault.this.id
   role_definition_id = data.azurerm_role_definition.key_vault_administrator.id
   principal_id       = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].object_id
+}
+
+resource "azurerm_federated_identity_credential" "aks_csi_driver_proget" {
+  name                = azurerm_resource_group.this.name
+  resource_group_name = azurerm_resource_group.this.name
+
+  audience  = ["api://AzureADTokenExchange"]
+  issuer    = azurerm_kubernetes_cluster.this.oidc_issuer_url
+  parent_id = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].client_id
+  subject   = "system:serviceaccount:proget:workload-identity-sa"
 }
