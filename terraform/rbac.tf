3986edd5deb1c5d807dc681029f0ac3a9967cb66
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-27 23:10:02 -0600

    Update principal_id in rbac.tf

--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -7,5 +7,5 @@ resource "azurerm_role_assignment" "tf_azure_rm" {
 resource "azurerm_role_assignment" "aks_csi_driver_managed_identity" {
   scope              = azurerm_key_vault.this.id
   role_definition_id = data.azurerm_role_definition.key_vault_administrator.id
-  principal_id       = azurerm_service_principal.this.object_id
+  principal_id       = azuread_service_principal.this.object_id
 }
