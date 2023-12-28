c82251ca2b6d58e64fbece2a091c8084b3ddf7b1
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-27 22:54:36 -0600

    Remove azuread_service_principal resource

--- terraform/base.tf
+++ terraform/base.tf
@@ -7,9 +7,3 @@ resource "azurerm_resource_group" "this" {
 
   location = "centralus"
 }
-
-resource "azuread_service_principal" "this" {
-  client_id                    = data.azuread_client_config.this.client_id
-  app_role_assignment_required = false
-  owners                       = [data.azuread_client_config.this.object_id]
-}
