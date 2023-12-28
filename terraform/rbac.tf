8ccad44171cecf7a6fc1c0b6a28606a16bd19861
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-28 10:39:53 -0600

    Update RBAC configuration for Azure resources

--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -1,11 +1,17 @@
-resource "azurerm_role_assignment" "tf_azure_rm" {
+resource "azurerm_role_assignment" "tf_azurerm" {
   scope              = azurerm_key_vault.this.id
   role_definition_id = data.azurerm_role_definition.key_vault_administrator.id
   principal_id       = data.azurerm_client_config.this.object_id
 }
 
+resource "azurerm_user_assigned_identity" "aks_csi_driver_managed_identity" {
+  name                = azurerm_resource_group.this.name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+}
+
 resource "azurerm_role_assignment" "aks_csi_driver_managed_identity" {
   scope              = azurerm_key_vault.this.id
   role_definition_id = data.azurerm_role_definition.key_vault_administrator.id
-  principal_id       = azuread_service_principal.this.object_id
+  principal_id       = azurerm_user_assigned_identity.aks_csi_driver_managed_identity.principal_id
 }
