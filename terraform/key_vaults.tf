4c97303d09ccbf0854af42d9a3432fe860b808e5
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-28 22:21:31 -0600

    Add dependency on azurerm_role_assignment.tf_azurerm

--- terraform/key_vaults.tf
+++ terraform/key_vaults.tf
@@ -15,4 +15,6 @@ resource "azurerm_key_vault_secret" "sql_connection_string" {
 
   key_vault_id = azurerm_key_vault.this.id
   value        = local.sql_connection_string
+
+  depends_on = [ azurerm_role_assignment.tf_azurerm ]
 }
