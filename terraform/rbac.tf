6eb4447acea619e6e530e65f20043edbbae1dd02
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-31 00:48:45 -0600

    Add RBAC role assignment for AKS CSI driver and application gateway ingress controller

--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -5,10 +5,16 @@ resource "azurerm_role_assignment" "tf_azurerm" {
 }
 
 # grant key vault admin role to the auto-create aks csi driver identity
-resource "azurerm_role_assignment" "auto_created_aks_csi_driver_identity" {
+resource "azurerm_role_assignment" "aks_csi_driver_identity" {
   scope              = azurerm_key_vault.this.id
   role_definition_id = data.azurerm_role_definition.key_vault_administrator.id
   principal_id       = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].object_id
+}
+
+resource "azurerm_role_assignment" "aks_application_gateway_ingress_controller" {
+  scope              = azurerm_subnet.app_gateway.id
+  role_definition_id = data.azurerm_role_definition.network_contributor.id
+  principal_id       = azurerm_kubernetes_cluster.this.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
 }
 
 # if not using the auto-created aks csi driver identity, create one and assign
