cd01a4283cb20a4db4096df2716a6d16b03301bc
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-30 20:41:57 -0600

    Commented out unused code

--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -4,17 +4,17 @@ resource "azurerm_role_assignment" "tf_azurerm" {
   principal_id       = data.azurerm_client_config.this.object_id
 }
 
-resource "azurerm_user_assigned_identity" "aks_csi_driver_identity" {
-  name                = azurerm_resource_group.this.name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-}
+# resource "azurerm_user_assigned_identity" "aks_csi_driver_identity" {
+#   name                = azurerm_resource_group.this.name
+#   resource_group_name = azurerm_resource_group.this.name
+#   location            = azurerm_resource_group.this.location
+# }
 
-resource "azurerm_role_assignment" "aks_csi_driver_identity" {
-  scope              = azurerm_key_vault.this.id
-  role_definition_id = data.azurerm_role_definition.key_vault_administrator.id
-  principal_id       = azurerm_user_assigned_identity.aks_csi_driver_identity.principal_id
-}
+# resource "azurerm_role_assignment" "aks_csi_driver_identity" {
+#   scope              = azurerm_key_vault.this.id
+#   role_definition_id = data.azurerm_role_definition.key_vault_administrator.id
+#   principal_id       = azurerm_user_assigned_identity.aks_csi_driver_identity.principal_id
+# }
 
 resource "azurerm_role_assignment" "auto_created_aks_csi_driver_identity" {
   scope              = azurerm_key_vault.this.id
@@ -22,12 +22,12 @@ resource "azurerm_role_assignment" "auto_created_aks_csi_driver_identity" {
   principal_id       = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].object_id
 }
 
-resource "azurerm_federated_identity_credential" "aks_csi_driver_proget" {
-  name                = azurerm_resource_group.this.name
-  resource_group_name = azurerm_resource_group.this.name
+# resource "azurerm_federated_identity_credential" "aks_csi_driver_proget" {
+#   name                = azurerm_resource_group.this.name
+#   resource_group_name = azurerm_resource_group.this.name
 
-  parent_id = azurerm_user_assigned_identity.aks_csi_driver_identity.id # replace(azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].user_assigned_identity_id, "resourcegroups", "resourceGroups")
-  issuer    = azurerm_kubernetes_cluster.this.oidc_issuer_url
-  subject   = "system:serviceaccount:proget:workload-identity-sa"
-  audience  = ["api://AzureADTokenExchange"]
-}
+#   parent_id = azurerm_user_assigned_identity.aks_csi_driver_identity.id # replace(azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].user_assigned_identity_id, "resourcegroups", "resourceGroups")
+#   issuer    = azurerm_kubernetes_cluster.this.oidc_issuer_url
+#   subject   = "system:serviceaccount:proget:workload-identity-sa"
+#   audience  = ["api://AzureADTokenExchange"]
+# }
