614da806a50b93c83ef21872c942bba35b309983
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-31 01:30:14 -0600

    Add scope definition to prevent re-creation of role assignments

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,11 +2,13 @@ data "azurerm_subscription" "this" {}
 
 data "azurerm_client_config" "this" {}
 
+# if this scope is not defined, it's assoicated role assignment(s) will be re-created on every run
 data "azurerm_role_definition" "key_vault_administrator" {
   name  = "Key Vault Administrator"
   scope = azurerm_key_vault.this.id
 }
 
+# if this scope is not defined, it's assoicated role assignment(s) will be re-created on every run
 data "azurerm_role_definition" "network_contributor" {
   name  = "Network Contributor"
   scope = azurerm_subnet.app_gateway.id
