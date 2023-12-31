8ae98f2e8da7e34c5409409a3163e16893e776dd
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-31 01:18:34 -0600

    Update role definition for Key Vault Administrator and Network Contributor

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,10 +4,11 @@ data "azurerm_client_config" "this" {}
 
 data "azurerm_role_definition" "key_vault_administrator" {
   name  = "Key Vault Administrator"
+  scope = azurerm_key_vault.this.id
 }
 
 data "azurerm_role_definition" "network_contributor" {
-  name  = "Network Contributor"
+  name = "Network Contributor"
 }
 
 # data "http" "tf_execution_ip" {
