42253aa2a56a0a46ae16ebc33bae5c7e15622099
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-30 21:48:20 -0600

    Add tf_execution_ip data source

--- terraform/data.tf
+++ terraform/data.tf
@@ -6,3 +6,7 @@ data "azurerm_role_definition" "key_vault_administrator" {
   name  = "Key Vault Administrator"
   scope = azurerm_key_vault.this.id
 }
+
+data "http" "tf_execution_ip" {
+  url = "https://ipv4.icanhazip.com"
+}
