3d7231e880082705f18ec1e8c2995f7a0a1ff266
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-30 21:58:05 -0600

    Commented out http data source for tf_execution_ip

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,6 +7,6 @@ data "azurerm_role_definition" "key_vault_administrator" {
   scope = azurerm_key_vault.this.id
 }
 
-data "http" "tf_execution_ip" {
-  url = "https://ipv4.icanhazip.com"
-}
+# data "http" "tf_execution_ip" {
+#   url = "https://ipv4.icanhazip.com"
+# }
