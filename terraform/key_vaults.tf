d38107283406d0041905b602350379fb47b80512
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-31 17:33:28 -0600

    Update key vault secret name

--- terraform/key_vaults.tf
+++ terraform/key_vaults.tf
@@ -18,7 +18,7 @@ resource "azurerm_key_vault" "this" {
 }
 
 resource "azurerm_key_vault_secret" "sql_connection_string" {
-  name = "${azurerm_resource_group.this.name}-sql-connection-string"
+  name = "sql-connection-string"
 
   key_vault_id = azurerm_key_vault.this.id
   value        = local.sql_connection_string
