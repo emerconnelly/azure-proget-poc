8001e6313941c223ba7d634ad07a180386e81db0
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-12-30 20:34:42 -0600

    Refactor RBAC configuration in terraform/rbac.tf

--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -16,11 +16,11 @@ resource "azurerm_role_assignment" "aks_csi_driver_identity" {
   principal_id       = azurerm_user_assigned_identity.aks_csi_driver_identity.principal_id
 }
 
-# resource "azurerm_role_assignment" "aks_csi_driver_identity" {
-#   scope              = azurerm_key_vault.this.id
-#   role_definition_id = data.azurerm_role_definition.key_vault_administrator.id
-#   principal_id       = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].object_id
-# }
+resource "azurerm_role_assignment" "auto_created_aks_csi_driver_identity" {
+  scope              = azurerm_key_vault.this.id
+  role_definition_id = data.azurerm_role_definition.key_vault_administrator.id
+  principal_id       = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].object_id
+}
 
 resource "azurerm_federated_identity_credential" "aks_csi_driver_proget" {
   name                = azurerm_resource_group.this.name
