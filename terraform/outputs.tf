9831bd4992b3dfb5791760afa7334e014159b24c
Emer Connelly <emer.connelly@wolterskluwer.com>
2024-01-01 01:53:41 -0600

    Remove csi_secret_store_identity output from terraform/outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,3 @@
-output "csi_secret_store_identity" {
-  value       = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity
-  description = "The identity used for the Secret Store CSI driver for Azure Key Vault."
-
-  sensitive = true
-}
-
 output "kubeconfig" {
   value       = azurerm_kubernetes_cluster.this.kube_config_raw
   description = "Kubeconfig for AKS cluster"
