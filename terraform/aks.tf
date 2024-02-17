d6054b315b2a1dc624a2fc230d9a0e50e8f84a5a
Emerson Connelly <emerconn7@gmail.com>
2024-02-17 00:49:10 -0600

    Update aks.tf

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -11,7 +11,7 @@ resource "azurerm_kubernetes_cluster" "this" {
 
   default_node_pool {
     name                        = "default"
-    vm_size                     = "Standard_D2lds_v5"
+    vm_size                     = "Standard_D2a_v4"
     node_count                  = 1
     os_sku                      = "AzureLinux"
     os_disk_type                = "Ephemeral"
